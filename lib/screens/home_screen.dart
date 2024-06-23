3177b75b4e77b8cd586a73500eee05571561a1f3
Namunllvo <nmhiey@gmail.com>
2024-06-24 00:48:14 +0900

    onResetPressed & auto formatting import error

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,23 @@
 import 'dart:async';
 import 'dart:ffi';
+// 언제 import 되었는지 모르겠는데
+// 아래 임포트 된것때문에 디버그 에러 발생
+// lib/screens/home_screen.dart:5:8: Error: Dart library 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart' is not available on this platform.
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+//        ^
+// Context: The unavailable library 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart' is imported through these packages:
+//     package:toonflix => dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart
+// Detailed import paths for (some of) the these imports:
+//     package:toonflix/main.dart => package:toonflix/screens/home_screen.dart => dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart
+// lib/screens/home_screen.dart:5:8: Error: Dart library 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart' is not available on this platform.
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+//        ^
+// Context: The unavailable library 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart' is imported through these packages:
+//     package:toonflix => dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart
+// Detailed import paths for (some of) the these imports:
+//     package:toonflix/main.dart => package:toonflix/screens/home_screen.dart => dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart
+
+// import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
 
 import 'package:flutter/material.dart';
 
@@ -50,6 +68,14 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  void onResetPressed() {
+    if (isRunning) return;
+    setState(() {
+      totalSeconds = twentyFiveMinutes;
+      // print('reset');
+    });
+  }
+
   String format(int seconds) {
     var duration = Duration(seconds: seconds);
     // print(duration.toString().split(".")[0].split("0:")[1]); ==25:00
@@ -80,15 +106,28 @@ class _HomeScreenState extends State<HomeScreen> {
           Flexible(
             flex: 4,
             child: Center(
-              child: IconButton(
-                iconSize: 200,
-                color: Theme.of(context).cardColor,
-                onPressed: isRunning ? onPausePressed : onStartPressed,
-                icon: Icon(
-                  isRunning
-                      ? Icons.motion_photos_pause_sharp
-                      : Icons.play_arrow_rounded,
-                ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  IconButton(
+                    iconSize: 200,
+                    color: Theme.of(context).cardColor,
+                    onPressed: isRunning ? onPausePressed : onStartPressed,
+                    icon: Icon(
+                      isRunning
+                          ? Icons.motion_photos_pause_sharp
+                          : Icons.play_arrow_rounded,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: onResetPressed,
+                    icon: Icon(
+                      isRunning ? Icons.lock_reset_sharp : Icons.refresh_sharp,
+                      color: Theme.of(context).cardColor,
+                    ),
+                    iconSize: 100,
+                  ),
+                ],
               ),
             ),
           ),
